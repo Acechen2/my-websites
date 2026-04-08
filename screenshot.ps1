e9714b125148d30928e5556857717176f5c66706
AceChen <202351519@qq.com>
2026-04-08 08:14:26 +0800

    refactor: 整理仓库，将练习项目移入my-websites文件夹，课件作为首页

--- screenshot.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$bmp = New-Object System.Drawing.Bitmap($screen.Width, $screen.Height)
-$g = [System.Drawing.Graphics]::FromImage($bmp)
-$g.CopyFromScreen($screen.Location, (New-Object System.Drawing.Point(0,0)), $screen.Size)
-$bmp.Save("c:\Users\DELL\WorkBuddy\Claw\desktop.png")
-$bmp.Dispose()
